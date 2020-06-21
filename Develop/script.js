1888c32b563a32272619ba08ab74bcd352b944aa
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-06-21 03:13:49 -0500

    added alerts if password does not meet criteria

--- Develop/script.js
+++ Develop/script.js
@@ -11,7 +11,12 @@ function generatePassword() {
   var passwordCriteria = "";
 
   var passwordLength = window.prompt("Enter the length of your password. Must be a number between 8 and 128");
-  
+  // Will return a warning if a valid number is not entered
+  if (isNaN(passwordLength) === true || passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be a valid number between 8 and 128");
+    return '';
+  };
+
   var lowercase = window.confirm("Does your password need to contain lowercase letters?");
   if (lowercase) {
     passwordCriteria += passwordCharacters.lowercase;
@@ -30,6 +35,12 @@ function generatePassword() {
   var symbols = window.confirm("Does your password need to contain symbols?");
   if (symbols) {
     passwordCriteria += passwordCharacters.symbol;
+  };
+
+  // Will return a warning if at least one password variable is not true
+  if (lowercase === false && uppercase === false && numbers === false && symbols === false) {
+    window.alert("You did not select any criteria for your password. Please click Generate Password and select at least one criteria for your password (i.e. lowercase, uppercase, numbers, or symbols).");
+    return '';
   };
 
   var password = "";
