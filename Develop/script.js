42ba9a89cec8fb18a3f5bc0f68f2bae7151d32a8
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-06-21 02:45:18 -0500

    generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,38 @@ var passwordCharacters = {
   symbol: '!@#$%^&*()-+=<>?[]{}',
 };
 
+// Generate password
+function generatePassword() {
+  var passwordCriteria = "";
+
+  var passwordLength = window.prompt("Enter the length of your password. Must be a number between 8 and 128");
+  
+  var lowercase = window.confirm("Does your password need to contain lowercase letters?");
+  if (lowercase) {
+    passwordCriteria += passwordCharacters.lowercase;
+  };
+
+  var uppercase = window.confirm("Does your password need to contain uppercase letters");
+  if (uppercase) {
+    passwordCriteria += passwordCharacters.uppercase;
+  };
+
+  var numbers = window.confirm("Does your password need to contain numbers?");
+  if (numbers) {
+    passwordCriteria += passwordCharacters.number;
+  };
+
+  var symbols = window.confirm("Does your password need to contain symbols?");
+  if (symbols) {
+    passwordCriteria += passwordCharacters.symbol;
+  };
+
+  var password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    password += passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)]
+  };
+  return password;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
